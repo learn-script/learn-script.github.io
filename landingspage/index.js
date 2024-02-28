85ec6f66c59a8f994d02b7e4b65f7d8b0f14a580
Moopey1 <tijn122@gmail.com>
2024-02-29 00:45:53 +0100

    html structuur functies

--- landingspage/index.js
+++ landingspage/index.js
@@ -1,8 +1,30 @@
-const body = document.querySelector('body');
+const app = document.querySelector('#app');
 const root = document.createElement('div');
 root.id = 'root';
-body.append(root);
+app.append(root);
 
+// adds content to target elements
+const divContent = (content, target) => {
+    console.log(content);
+    content.forEach(element => {
+        console.log(element);
+        const content = document.createElement(element);
+        target.append(content);
+        if (element === 'img' && target.id === 'banner') {
+            const img = document.querySelector('#banner img');
+            img.setAttribute('src', 'roads-technology.png');
+        } else if (element === 'div' && target.id === 'banner') {
+            const buttonWrapper = document.querySelector('#banner div');
+            buttonWrapper.append(document.createElement('button'));
+            buttonWrapper.id = 'buttonWrapper';
+            const button = document.querySelector('#buttonWrapper button');
+            button.innerText = 'Menu';
+        }
+    });
+
+};
+
+// creates html tags and calls function to further add content to tags
 const createStructure = () => {
     const elements = ['header', 'main', 'footer'];
     for (const element of elements) {
@@ -14,40 +36,29 @@ const createStructure = () => {
     }
 };
 
+// creates elements and corresponding ID's 
+// for each element divContent() is called to futher add content to those elements
 const headerStructure = (header) => {
-    const structures = [
-        { id: 'banner' },
-        { id: 'welcome' },
-        { id: 'info' }
-    ];
+    const structures = ['banner', 'welcome', 'info'];
+    const bannerElements = ['img', 'div'];
+
     structures.forEach(structure => {
         const newDiv = document.createElement('div');
-        newDiv.id = structure.id;
-        if (structure.id == 'banner') {
-            bannerContent(newDiv);
-        }
+        newDiv.id = structure;
         header.append(newDiv);
+        switch (structure) {
+            case 'banner':
+                const target = document.querySelector('#banner');
+                console.log(target);
+                divContent(bannerElements, target);
+                break;
+        }
+
     });
+
 };
 
-const bannerContent = (banner) => {
-    const bannerElements = ['img', 'div', 'button'];
-
-    // const img = document.createElement('img');
-    // const buttonWrapper = document.createElement('div');
-    // const button = document.createElement('button');
-
-    bannerElements.forEach(element => {
-        const content = document.createElement(element);
-        if(element !== 'button'){
-            banner.append(content);
-        } else if (element == 'div') {
-            element.append(bannerElements[2]);
-        }
-
-    })
-    // img.setAttribute('src', 'roads-technology.png');
-    // banner.append(img, buttonWrapper);
-    // buttonWrapper.append(button.innerText = 'Menu');
-}
+// img.setAttribute('src', 'roads-technology.png');
+// banner.append(img, buttonWrapper);
+// buttonWrapper.append(button.innerText = 'Menu');
 createStructure();
